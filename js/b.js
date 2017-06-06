e85aad7fc2313f823faead53e631ada991b33328
user <brewbaker@yandex.com>
2017-06-06 12:58:55 +0300

    Create b.js

---
+++ js/b.js
@@ -0,0 +1,21 @@
+ChatraID = 'rh5rJer2fF3PaKHeb';
+    (function(d, w, c) {
+        var n = d.getElementsByTagName('script')[0],
+            s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+            + '//call.chatra.io/chatra.js';
+        n.parentNode.insertBefore(s, n);
+    })(document, window, 'Chatra');
+
+window.ChatraSetup = {
+    mode: 'frame',
+    injectTo: 'bot' 
+};
+
+   Chatra('setColors', {
+    chatBg: 'transparent'
+});
