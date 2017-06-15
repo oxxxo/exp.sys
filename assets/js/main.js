e96f0d26f5fea2a7c2046426ecb56b954589da6c
user <brewbaker@yandex.com>
2017-06-16 00:56:40 +0300

    Create main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,7 @@ jQuery(document).ready(function($) {
         entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
         
         }
-    );
+  //  );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
     GitHubCalendar(".calendar", "cepbep");
