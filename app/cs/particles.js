99ccb39e1ecaf91576b0be968dacd404e4905901
user <brewbaker@yandex.com>
2017-09-05 03:14:44 +0300

    Create particles.js

---
+++ app/cs/particles.js
@@ -0,0 +1,110 @@
+{
+  "particles": {
+    "number": {
+      "value": 29,
+      "density": {
+        "enable": true,
+        "value_area": 1683.5826639087988
+      }
+    },
+    "color": {
+      "value": "#fff"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "http://findicons.com/files/icons/329/simple/128/open_folder.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 16.03412060865523,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 7.308694910712106,
+        "size_min": 0,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 500,
+      "color": "#ffffff",
+      "opacity": 0.02405118091298284,
+      "width": 2
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.603412060865523,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 1683.5826639087988,
+        "rotateY": 1763.753266952075
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 0.5
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 4,
+        "duration": 0.3,
+        "opacity": 1,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
