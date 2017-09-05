f76cf580a3822d3abb541c81a94d1319c5f79b9d
user <brewbaker@yandex.com>
2017-09-05 03:26:50 +0300

    Update app.js

--- app/cs/js/app.js
+++ app/cs/js/app.js
@@ -1,5 +1,5 @@
 /* -----------------------------------------------
-/* dgfdggdgg/dfhdd/d
+/* dgfdggdgg/dfhdd/
 /* ----------------------------------------------- */
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
